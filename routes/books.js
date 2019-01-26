f5dc59134cdf854b00d979ea74f531299bc6d162
Joseph Okpara <joejr.okpara@gmail.com>
2019-01-26 11:57:31 -0600

    Now we are talking to the database.

---
+++ routes/books.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var router = express.Router();
+
+mongoose.connect('mongodb://localhost/library');
+var db = mongoose.connection;
+
+db.on('error', function(msg) {
+    console.log('Mongoose connection error %s', msg);
+});
+
+db.once('open', function(){
+    console.log('Mongoose connection established');
+})
+
+var Book = require('../models/book');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.json({"status":"Success"})
+});
+
+router.get('/all', function(req,res, next){
+    Book.find({}).exec(function(err, data){
+        if(err){
+            console.log(err);
+            res.status(500);
+        }
+        else {
+            res.json({data: data});
+        }
+    });
+})
+module.exports = router;
